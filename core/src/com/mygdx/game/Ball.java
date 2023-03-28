de7fbb7b840e46535d5b2af04baa396df4ad068c
dexffe <80205120+dexffe@users.noreply.github.com>
2023-03-29 01:01:41 +0600

    Update Ball.java

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -7,21 +7,12 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-<<<<<<< HEAD
 
-public class Ball {
-    Body body;
-    Ball(World world, float x, float y, float radius) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-=======
 public class Ball {
     Body body;
     Ball(World world, float x, float y, float radius){
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
->>>>>>> origin/master
         bodyDef.position.set(x, y);
 
         body = world.createBody(bodyDef);
@@ -33,20 +24,10 @@ public class Ball {
         fixtureDef.shape = circle;
         fixtureDef.density = 0.5f;
         fixtureDef.friction = 0.4f;
-<<<<<<< HEAD
-        fixtureDef.restitution = 0.8f; // Make it bounce a little bit
-=======
         fixtureDef.restitution = 0.6f;
->>>>>>> origin/master
 
         Fixture fixture = body.createFixture(fixtureDef);
 
         circle.dispose();
-<<<<<<< HEAD
     }
 }
-=======
-
-    }
-}
->>>>>>> origin/master
